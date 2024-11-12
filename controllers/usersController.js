5f1fc01edc310ff084296fd045afaa64652dcf6b
PopescuStefan1 <stefan.ro95@gmail.com>
2024-11-12 19:44:37 +0200

    Add more fields to form with validation

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,32 @@
 // controllers/usersController.js
 import usersStorage from "../storages/usersStorage.js";
+import { body, validationResult } from "express-validator";
+
+const alphaErr = "must only contain letters.";
+const lengthErr = "must be between 1 and 10 characters.";
+const formatErr = "must be formatted correctly.";
+
+const validateUser = [
+  body("firstName")
+    .trim()
+    .isAlpha()
+    .withMessage(`First name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`First name ${lengthErr}`),
+  body("lastName")
+    .trim()
+    .isAlpha()
+    .withMessage(`Last name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`Last name ${lengthErr}`),
+  body("email").trim().isEmail().withMessage(`Email ${formatErr}`),
+  body("age")
+    .trim()
+    .isInt({ min: 18, max: 120 })
+    .withMessage("Age must be a number between 18 and 120")
+    .optional({ nullable: true, checkFalsy: true }),
+  body("bio").trim().isLength({ max: 200 }).withMessage("Bio must be between 0 and 200 characters"),
+];
 
 export function usersListGet(req, res) {
   res.render("index", {
@@ -14,8 +41,51 @@ export function usersCreateGet(req, res) {
   });
 }
 
-export function usersCreatePost(req, res) {
-  const { firstName, lastName } = req.body;
-  usersStorage.addUser({ firstName, lastName });
+export const usersCreatePost = [
+  validateUser,
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("createUser", {
+        title: "Create user",
+        errors: errors.array(),
+      });
+    }
+    const { firstName, lastName, email, age, bio } = req.body;
+    usersStorage.addUser({ firstName, lastName, email, age, bio });
+    res.redirect("/");
+  },
+];
+
+export const usersUpdateGet = (req, res) => {
+  const user = usersStorage.getUser(req.params.id);
+  res.render("updateUser", {
+    title: "Update user",
+    user: user,
+  });
+};
+
+export const usersUpdatePost = [
+  validateUser,
+  (req, res) => {
+    const user = usersStorage.getUser(req.params.id);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("updateUser", {
+        title: "Update user",
+        user: user,
+        errors: errors.array(),
+      });
+    }
+    const { firstName, lastName, email, age, bio } = req.body;
+    console.log(req.body);
+    usersStorage.updateUser(req.params.id, { firstName, lastName, email, age, bio });
+    res.redirect("/");
+  },
+];
+
+// Tell the server to delete a matching user, if any. Otherwise, respond with an error.
+export const usersDeletePost = (req, res) => {
+  usersStorage.deleteUser(req.params.id);
   res.redirect("/");
-}
+};
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,10 +1,20 @@
 // routes/usersRouter.js
 import { Router } from "express";
-import { usersListGet, usersCreateGet, usersCreatePost } from "../controllers/usersController.js";
+import {
+  usersListGet,
+  usersCreateGet,
+  usersCreatePost,
+  usersUpdateGet,
+  usersUpdatePost,
+  usersDeletePost,
+} from "../controllers/usersController.js";
 const usersRouter = Router();
 
 usersRouter.get("/", usersListGet);
 usersRouter.get("/create", usersCreateGet);
 usersRouter.post("/create", usersCreatePost);
+usersRouter.get("/:id/update", usersUpdateGet);
+usersRouter.post("/:id/update", usersUpdatePost);
+usersRouter.post("/:id/delete", usersDeletePost);
 
 export default usersRouter;
